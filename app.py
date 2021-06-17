1505aac801b8a0d0dddb317847b2386e06dd15d5
Nadhem001 <54727153+Nadhem001@users.noreply.github.com>
2021-06-17 09:36:43 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,49 @@
 from flask import Flask, render_template,Response
 import serial
-from flask_socketio import SocketIO,send,emit
+from flask_socketio import SocketIO,send,emit,disconnect
 import time
-import cv2
-arduino = serial.Serial('COM4',9600)
+from cv2 import cv2
+arduino = serial.Serial('/dev/ttyACM0',9600)
+#arduino = serial.Serial('COM4',9600)
 app=Flask(__name__)
+camera = cv2.VideoCapture(0)
 socketio = SocketIO(app, cors_allowed_origins='*')
 @socketio.on('connect')
 def test_control():
     print("nadhem connecte")
 
+
 @socketio.on('get')
-def test_control1(a):
-    print(a['status'])
+def marche(a):
+    print("aaaaaaaaaa,,,,,,,,,,,,,,,,,,aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     msg=a['status']
-    arduino.write(msg.encode())
-
-
+    arduino.write(msg.encode()) 
+    print(a['status'])
+    socketio.emit("b",1)
+    line = arduino.readline().decode('utf-8').rstrip()
+    print(line)
+    time.sleep(0.1)     
 @app.route('/')
 def hello_world():
-    msg='F'
-    arduino.write(msg.encode())
-    socketio.emit("b",1)
+   # arduino.write(msg.encode())
+   # socketio.emit("b",1)
+    
 
-    """Video streaming route. Put this in the src attribute of an img tag."""
     return render_template('index.html')
-
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+		 # concat frame one by one and show result
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-def gen():
-    """Video streaming generator function."""
-    cap = cv2.VideoCapture('http://192.168.1.18:8080/video')
-
-    # Read until video is completed
-    while (cap.isOpened()):
-        # Capture frame-by-frame
-        ret, img = cap.read()
-        if ret == True:
-            img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-            frame = cv2.imencode('.jpg', img)[1].tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            time.sleep(0.1)
-        else:
-            break
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
