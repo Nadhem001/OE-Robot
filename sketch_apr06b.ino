e2e1d4e4e1b7e349934b9de1f2a803f5f618c856
Nadhem001 <54727153+Nadhem001@users.noreply.github.com>
2021-06-17 09:37:44 +0100

    Update sketch_apr06b.ino

--- sketch_apr06b.ino
+++ sketch_apr06b.ino
@@ -17,25 +17,35 @@
 #define IN4 11
 
 char value;
+boolean avance=false;
+boolean recule=false;
+
+int Echo = A4;
+int Trig = A5;
+
+int Echo2 = A1;
+int Trig2 = A0;
 
 void forward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH); 
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA,100);
+  analogWrite(ENB,100);
+  Serial.println("forward-Arduino");
+  delay(50);
+}
+
+void back() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
   analogWrite(ENA,100);
   analogWrite(ENB,100);
-  Serial.println("forward");
-}
-
-void back() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH); 
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA,80);
-  analogWrite(ENB,80);
-  Serial.println("back");
+  Serial.println("Back-Arduino");
+  delay(50);
 }
 
 void left() {
@@ -43,9 +53,10 @@ void left() {
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA,150);
-  analogWrite(ENB,150);
-  Serial.println("left");
+  analogWrite(ENA,220);
+  analogWrite(ENB,220);
+  Serial.println("left-Arduino");
+  delay(50);
 }
 
 void right() {
@@ -53,12 +64,13 @@ void right() {
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA,150);
-  analogWrite(ENB,150);
-  Serial.println("right");
+  analogWrite(ENA,220);
+  analogWrite(ENB,220);
+  Serial.println("right-Arduino");
+delay(50);
 }
 
-void pause() {
+void stop() {
   digitalWrite(ENA, HIGH);
   digitalWrite(ENB, HIGH);
   digitalWrite(IN1, LOW);
@@ -66,8 +78,45 @@ void pause() {
   
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
-  Serial.println("pause");
+  Serial.println("stop-Arduino");
+  delay(50);}
+  
+long lecture_echo;
+long cm;
+//Ultrasonic distance measurement Sub function
+int getDistance() {
+    digitalWrite(Trig, HIGH);
+ delayMicroseconds(40);
+ digitalWrite(Trig, LOW);
+ lecture_echo = pulseIn(Echo, HIGH);
+ cm = lecture_echo / 58;
+  Serial.println("bbbbbbbb");
+
+   Serial.println(cm);
+  
+    return cm;
+    
 }
+
+long lecture_echo2;
+long CM2;
+//Ultrasonic distance measurement Sub function
+int getDistance2() {
+    digitalWrite(Trig2, HIGH);
+ delayMicroseconds(50);
+ digitalWrite(Trig2, LOW);
+ lecture_echo2 = pulseIn(Echo2, HIGH);
+ 
+ CM2 = lecture_echo2 / 58;
+ Serial.println("aaaaaaaaaaa");
+
+   Serial.println(CM2);
+
+    return CM2;
+   
+}
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -77,30 +126,68 @@ void setup() {
   pinMode(IN4,OUTPUT);
   pinMode(ENA,OUTPUT);
   pinMode(ENB,OUTPUT);
+  pinMode(Echo, INPUT);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo2, INPUT);
+  pinMode(Trig2, OUTPUT);
+
+
 }
 
 void loop() {
-   
-    value = Serial.read();
-
+ 
+    value=Serial.read();
+     
     if (value == 'L') {
-      Serial.println("Left");
       left();
-    }
+      delay(10);
+      }
+      
     if (value == 'R') {
-      Serial.println("Right");
       right();
-    }
-    if (value == 'F') {
-      Serial.println("Forward");
-      forward();
-    }
-    if (value == 'B') {
-      Serial.println("Backward");
+     // delay(10);
+      }
+
+
+
+        if (value == 'B'){
+     if ((getDistance() > 20)||(getDistance()==0)) {
       back();
-    }
-      if (value == 'P') {
-      Serial.println("pause");
-     pause();
-    }
+      recule=true;
+      //delay(10); 
+      }   }
+      
+  if((getDistance() < 20)&&(recule==true))
+  {
+  
+    stop();  
+   Serial.println("stop-1");
+      recule=false;
+      //delay(10);
+      }
+
+
+      
+        if (value == 'F'){
+     if ((getDistance2() > 20)||(getDistance2()==0)) {
+      forward();
+      avance=true;
+      //delay(10); 
+      }
+      }
+      
+  if((getDistance2() < 20)&&(avance==true)&&(getDistance2() >0))
+  {
+    stop();  
+      avance=false;
+      //delay(10); 
+      }
+
+      
+    
+      if (value == 's') {
+     stop();
+     //delay(10);
+     }
+delay(100);
 }
